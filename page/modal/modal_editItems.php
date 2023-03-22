9790b0f7cac7bf8c7f08268d930f236e1a4055b2
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2023-03-22 22:42:44 +0700

    Add files via upload

--- page/modal/modal_editItems.php
+++ page/modal/modal_editItems.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="modal fade" id="modalEditItems<?php //echo $userAccount['id_users']; ?>" tabindex="-1" aria-labelledby="modalEditItems" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -30,37 +29,4 @@
             </div>
         </div>
     </div>
-=======
-<div class="modal fade" id="modalEditItems<?php //echo $userAccount['id_users']; ?>" tabindex="-1" aria-labelledby="modalEditItems" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalEditItems"><i class="fa-solid fa-pen-to-square"></i> แก้ไขข้อมูลรายการสินค้า (Edit Items)</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <div class="modal-body">
-                <form action="#" method="POST">
-                    <div class="mb-0">
-                        <input type="hidden" readonly value="<?php //echo $userAccount['id_users']; ?>" required class="form-control" name="id_users">
-                        <label for="itemsName" class="col-form-label">ชื่อรายการ :</label>
-                        <input type="text" value="<?php //echo $userAccount['name']; ?>" class="form-control" name="itemsName" required>
-                    </div>
-                    <div class="mb-0">
-                        <label for="editItemsType" class="col-form-label">ประเภทรายการ :</label>
-                        <select class="form-select" aria-label="editItemsType" name="editItemsType" required>
-                            <option>ค่าแรง</option>
-                            <option>ค่าวัสดุ</option>
-                        </select>
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary" name="editItems"><i class="fa-solid fa-floppy-disk"></i> Save</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
->>>>>>> danai
 </div>
--- page/modal/modal_editPassword.php
+++ page/modal/modal_editPassword.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="modal fade" id="modalPassword<?php //echo $userAccount['id_users']; ?>" tabindex="-1" aria-labelledby="modalPassword" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -31,38 +30,4 @@
             </div>
         </div>
     </div>
-=======
-<div class="modal fade" id="modalPassword<?php //echo $userAccount['id_users']; ?>" tabindex="-1" aria-labelledby="modalPassword" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalPassword"><i class="fa-solid fa-pen-to-square"></i> แก้ไขรหัสผ่าน (Edit Password)</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <div class="modal-body">
-                <form action="#" method="POST">
-                    <div class="mb-0">
-                        <label for="text" class="col-form-label font-weight-normal"><i class="fa-solid fa-circle-user"></i> ชื่อผู้ใช้ :</label>
-                        <input type="text" value="<?php //echo $userAccount['email']; ?>" required class="form-control" disabled>
-                    </div>
-                    <hr>
-                    <div class="mb-0">
-                        <label for="password" class="col-form-label font-weight-normal"><i class="fa-solid fa-key"></i> รหัสผ่าน (อย่างน้อย 8 ตัวอักษร) :</label>
-                        <input type="password" minlength="8" required class="form-control" name="password">
-                    </div>
-                    <div class="mb-0">
-                        <label for="confirmpassword" class="col-form-label font-weight-normal"><i class="fa-solid fa-key"></i> ยืนยันรหัสผ่าน :</label>
-                        <input type="password" minlength="4" required class="form-control" name="confirmPassword">
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-dark" name="editUser"><i class="fa-solid fa-floppy-disk"></i> Save</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
->>>>>>> danai
 </div>
