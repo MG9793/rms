0e6e508117674503af3acbd10abe89de93e3468f
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2023-03-26 16:50:33 +0700

    Add files via upload

--- page/include/navbar.php
+++ page/include/navbar.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg text-light px-4 bg-dark">
+<div class="navbar navbar-expand-lg text-light px-4 bg-dark">
     <div class="container">
-            <a class="navbar-brand"><img src="../image/logo/logo.jpg" class="rounded" style="width:80px"></a>
+        <a class="navbar-brand"><img src="../image/logo/logo.jpg" class="rounded" style="width:80px"></a>
             <h5 class="text-light">สิทธิชัย เอนจิเนียริ่ง - ระบบบริหารจัดการใบเสร็จ</h5>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -43,7 +43,7 @@
                             <li><a class="dropdown-item text-black disabled lh-1" style="font-size: 15px;"><i class="fa-solid fa-user"></i> <?php echo $userName_query['name'] . ' ' . $userName_query['lastname']; ?></a></li>
                             <li><a class="dropdown-item text-black disabled lh-1" style="font-size: 15px;"><i class="fa-solid fa-clock"></i> <span id="date"></span> <span id="clock"></span> </a></li>
                             <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item text-black" href="#" data-bs-toggle="modal" data-bs-target="#modalPassword<?php //echo $userAccount['id_users']; ?>"><i class="fa-solid fa-key"></i> เปลี่ยนรหัสผ่าน</a></li>
+                            <li><a class="dropdown-item text-black" href="#" data-bs-toggle="modal" data-bs-target="#modalPassword<?php echo $userName_query['username']; ?>"><i class="fa-solid fa-key"></i> เปลี่ยนรหัสผ่าน</a></li>
                             <li><a class="dropdown-item text-black" href="../db/config/logout.php"><i class="fa-solid fa-right-from-bracket"></i> ออกจากระบบ</a></li>
                         </ul>
                     </li>
@@ -51,4 +51,42 @@
             </div>
             <script type="text/javascript" src="../resources/js/displayDateTime.js"></script>
         </div>
-    </nav>
+    </div>
+</nav>
+
+
+
+<!-- Edit Password ห้ามลบ -->
+<div class="modal fade" id="modalPassword<?php echo $userName_query['username']; ?>" tabindex="-1" aria-labelledby="modalPassword" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalPassword"><i class="fa-solid fa-pen-to-square"></i> แก้ไขรหัสผ่าน (Edit Password)</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+                <form action="../db/db_userManagement.php" method="POST">
+                    <div class="mb-0">
+                        <label for="text" class="col-form-label font-weight-normal">ชื่อผู้ใช้ :</label>
+                        <input type="text" class="form-control" name="getUsername" value="<?php echo $userName_query['username']; ?>" required readonly>
+                    </div>
+                    <hr>
+                    <div class="mb-0">
+                        <label for="password" class="col-form-label font-weight-normal">รหัสผ่าน (อย่างน้อย 8 ตัวอักษร) :</label>
+                        <input type="password" class="form-control" name="password" id="password" minlength="8" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="confirmPassword" class="col-form-label font-weight-normal">ยืนยันรหัสผ่าน :</label>
+                        <input type="password" class="form-control" name="confirmPassword" id="confirmPassword" minlength="8" required>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-dark" name="editPassword"><i class="fa-solid fa-floppy-disk"></i> Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
