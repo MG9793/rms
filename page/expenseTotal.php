3ebb5bde1e0282a7b22e34e8e7c9837983e43584
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2023-03-27 21:14:22 +0700

    Add files via upload

---
+++ page/expenseTotal.php
@@ -0,0 +1,204 @@
+<?php
+    session_start();
+    require_once "../db/config/conn.php";
+    require_once "../db/config/deleteRow.php";
+
+    if (!isset($_SESSION['admin_login'])) {
+        header("location: ../login.php");
+    } else {
+
+        // query ชื่อผู้ใช้งาน
+        $id = $_SESSION['admin_login'];
+        $stmt = $conn->query("SELECT name, lastname, username FROM user_info WHERE id = $id");
+        $stmt->execute();
+        $userName_query = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>บันทึกค่าใช้จ่าย (ยอดรวม) | ระบบบริหารจัดการใบเสร็จ</title>
+
+    <!-- Dependency ห้ามลบ -->
+    <?php include "include/dependency.php"; ?>
+
+    <!-- Font kanit-300 ห้ามเอาออก -->
+    <style>
+        @font-face {
+            font-display: swap;
+            font-family: 'Kanit';
+            font-style: normal;
+            font-weight: 300;
+            src: url('../resources/fonts/kanit-v12-latin_thai-300.eot'); /* IE9 Compat Modes */
+            src: url('../resources/fonts/kanit-v12-latin_thai-300.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+                url('../resources/fonts/kanit-v12-latin_thai-300.woff2') format('woff2'), /* Super Modern Browsers */
+                url('../resources/fonts/kanit-v12-latin_thai-300.woff') format('woff'), /* Modern Browsers */
+                url('../resources/fonts/kanit-v12-latin_thai-300.ttf') format('truetype'), /* Safari, Android, iOS */
+                url('../resources/fonts/kanit-v12-latin_thai-300.svg#Kanit') format('svg'); /* Legacy iOS */
+        }
+
+        .col a {
+            text-decoration: none;
+            color: #000;
+        }
+
+        .col a:hover {
+            color: #000;
+        }
+
+        .card {
+            transition: transform .3s;
+        }
+
+        .card:hover {
+          background-color: lightyellow;
+          transform: scale(1.05);
+        }
+    </style>
+</head>
+<body>
+
+    <!-- navbar ห้ามลบ -->
+    <?php include 'include/navbar.php'; ?>
+
+    <!-- pagename ห้ามลบ -->
+    <section class="container mt-2">
+        <legend class="fw-bold text-dark text-center border border-3 border-light bg-secondary shadow-sm p-2"><i class="fa-solid fa-1 border rounded p-1 bg-dark text-light"></i> บันทึกค่าใช้จ่าย (ยอดรวม)</legend>
+    </section>
+
+    <!-- input ห้ามลบ -->
+    <section class="container">
+        <div class="row">
+            <div class="col-md center-screen">
+                <div class="row row-cols-2 row-cols-md-6 g-2">
+
+                    <?php
+
+                        $stmt = $conn->query("SELECT site_name FROM site_info");
+                        $stmt->execute();
+                        $allSite = $stmt->fetchAll();
+
+                        foreach($allSite as $fetch_allSite) {
+                    ?>
+
+                    <div class="col">
+                        <a href="#" data-bs-toggle="modal" data-bs-target="#selectVAT">
+                            <div class="card h-100 shadow dashboard">
+                                <div class="card-body text-center">
+                                    <img src="../image/icon/construction.png" class="mx-auto d-block mb-3 w-50">
+                                    <h5 class="card-title fw-bold mt-2"><?php echo $fetch_allSite['site_name']; ?></h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- Modal 1 -->
+    <div class="modal fade" id="selectVAT" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"><i class="fa-solid fa-2 border rounded p-2 bg-dark text-light"></i> เลือกประเภทการคำนวณภาษี (VAT)</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="#" method="POST">
+                        <div class="mb-0 text-center">
+                            <img src="../image/icon/tax.png" class="w-25" alt=""><br>
+                            <input type="hidden" readonly value="<?php //echo $userAccount['id_users']; ?>" required class="form-control" name="id_site">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-success w-100" data-bs-toggle="modal" data-bs-target="#expenseDetailsVAT">VAT</button>
+                    <button type="submit" class="btn btn-danger w-100" data-bs-toggle="modal" data-bs-target="#MA_Head_NoVAT">No VAT</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Modal 2 -->
+    <div class="modal fade" id="expenseDetailsVAT" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"><i class="fa-solid fa-3 border rounded p-2 bg-dark text-light"></i> บันทึกค่าใช้จ่ายรวมภาษี (VAT)</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="#" method="POST">
+                        <!-- <div class="text-center"><img src="../image/icon/cost.png" class="w-25" alt=""></div> -->
+                        <input type="hidden" readonly value="<?php //echo $userAccount['id_users']; ?>" required class="form-control" name="id_site">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label class="form-label fw-bold" for="receiptNo">เลขที่ใบเสร็จ :</label>
+                                <input type="text" class="form-control" placeholder="กรุณากรอกเลขที่ใบเสร็จ..." name="receiptNo" id="receiptNo" required>
+                            </div>
+                            <div class="col-md-4">
+                                <label class="form-label fw-bold" for="buyDate">วันที่ซื้อ :</label>
+                                <input type="date" class="form-control" name="buyDate" id="buyDate" required>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-6">
+                                <label class="form-label fw-bold" for="sellerName">ชื่อผู้ขาย :</label>
+                                <input type="text" class="form-control" name="sellerName" id="sellerName" placeholder="กรุณากรอกชื่อผู้ขาย..." required>
+                            </div>
+                            <div class="col-md-4">
+                                <label class="form-label fw-bold" for="taxID">เลขประจำตัวผู้เสียภาษี :</label>
+                                <input type="text" class="form-control" name="taxID" id="taxID" placeholder="กรุณากรอกเลขประจำตัวผู้เสียภาษี..." required>
+                            </div>
+                        </div>
+                        <div>หมายเหตุ : หากไม่พบชื่อผู้ขายให้ไปที่เมนูตั้งค่า และเพิ่มรายการผู้ขาย</div>
+
+                        <hr class="headerUnderline mt-4">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <label class="col-form-label fw-bold">ยอดรวม :</label>
+                                <input type="number" class="form-control" name="expenseTotal" id="expenseTotal" list="expenseTotal" oninput="calculateVAT()" required>
+                            </div>
+                            <div class="col-md-2">
+                                <label class="col-form-label fw-bold">+ VAT 7% :</label>
+                                <input type="number" class="form-control" name="expenseVAT" id="expenseVAT" list="expenseVAT" required disabled>
+                            </div>
+                            <div class="col-md-4">
+                                <label class="col-form-label fw-bold">รวมสุทธิ :</label>
+                                <input type="number" class="form-control" name="expenseSUM" id="expenseSUM" list="expenseSUM" required disabled>
+                            </div>
+                        </div>
+
+                        <div class="row mt-4">
+                            <div class="col-md">
+                                <button type="submit" class="btn btn-dark text-light w-100"><i class="fa-solid fa-floppy-disk"></i> Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function calculateVAT() {
+            var price = document.getElementById("expenseTotal").value;
+            var vat = price * 0.07;
+            document.getElementById("expenseVAT").value = vat.toFixed(2);
+            document.getElementById("expenseSUM").value = (parseFloat(price) + vat).toFixed(2);
+        }
+    </script>
+
+</body>
+</html>
+
+<?php } ?>
---
+++ page/siteManagement.php
@@ -0,0 +1,239 @@
+<?php
+    session_start();
+    require_once "../db/config/conn.php";
+    require_once "../db/config/deleteRow.php";
+
+    if (!isset($_SESSION['admin_login'])) {
+        header("location: ../login.php");
+    } else {
+
+        // query ชื่อผู้ใช้งาน
+        $id = $_SESSION['admin_login'];
+        $stmt = $conn->query("SELECT name, lastname, username FROM user_info WHERE id = $id");
+        $stmt->execute();
+        $userName_query = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>จัดการไซต์งาน (Site Management) | ระบบบริหารจัดการใบเสร็จ</title>
+
+    <!-- Dependency ห้ามลบ -->
+    <?php include "include/dependency.php"; ?>
+
+    <!-- Font kanit-300 ห้ามเอาออก -->
+    <style>
+        @font-face {
+            font-display: swap;
+            font-family: 'Kanit';
+            font-style: normal;
+            font-weight: 300;
+            src: url('../resources/fonts/kanit-v12-latin_thai-300.eot'); /* IE9 Compat Modes */
+            src: url('../resources/fonts/kanit-v12-latin_thai-300.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+                url('../resources/fonts/kanit-v12-latin_thai-300.woff2') format('woff2'), /* Super Modern Browsers */
+                url('../resources/fonts/kanit-v12-latin_thai-300.woff') format('woff'), /* Modern Browsers */
+                url('../resources/fonts/kanit-v12-latin_thai-300.ttf') format('truetype'), /* Safari, Android, iOS */
+                url('../resources/fonts/kanit-v12-latin_thai-300.svg#Kanit') format('svg'); /* Legacy iOS */
+        }
+    </style>
+</head>
+<body>
+
+    <!-- navbar ห้ามลบ -->
+    <?php include 'include/navbar.php'; ?>
+    
+    <!-- pagename ห้ามลบ -->
+    <section class="container mt-2">
+        <legend class="fw-bold text-dark text-center border border-3 border-light bg-secondary shadow-sm p-2">จัดการไซต์งาน (Site Management)</legend>
+    </section>
+
+    <!-- Alert ห้ามลบ -->
+    <section class="container">
+        <div class="row">
+            <div class="col-md">
+                <?php
+                    // Alert เพิ่มรายการสำเร็จ
+                    if(isset($_SESSION['addSite_success'])) {
+                        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>";
+                        echo "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>";
+                        echo $_SESSION['addSite_success'];
+                        unset($_SESSION['addSite_success']);
+                        echo "</div>";
+                    }
+
+                    // Alert แก้ไขรายการสำเร็จ
+                    else if(isset($_SESSION['editSite_success'])) {
+                        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>";
+                        echo "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>";
+                        echo $_SESSION['editSite_success'];
+                        unset($_SESSION['editSite_success']);
+                        echo "</div>";
+                    }
+
+                    // Alert ลบรายการสำเร็จ
+                    else if(isset($_SESSION['deleteSite_success'])) {
+                        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>";
+                        echo "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>";
+                        echo $_SESSION['deleteSite_success'];
+                        unset($_SESSION['deleteSite_success']);
+                        echo "</div>";
+                    }
+
+                    // Alert รายการซ้ำ
+                    else if(isset($_SESSION['addSite_error'])) {
+                        echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>";
+                        echo "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>";
+                        echo $_SESSION['addSite_error'];
+                        unset($_SESSION['addSite_error']);
+                        echo "</div>";
+                    }
+                ?>
+            </div>
+        </div>
+    </section>
+
+    <!-- input ห้ามลบ -->
+    <section class="container">
+        <div class="collapse show" id="collapseForm">
+            <fieldset class="p-3 shadow-sm mt-2">
+                <h5 class="fw-bold"><i class="fa-solid fa-plus"></i> เพิ่มไซต์งาน</h5>
+
+                <form action="../db/db_siteManagement.php" method="POST">
+                    <div class="row mb-3">
+                        <div class="col-md-8">
+                            <label for="siteName" class="form-label fw-bold">ชื่อไซต์งาน :</label>
+                            <input type="text" class="form-control" name="siteName" id="siteName" placeholder="ไซต์งาน.." required>
+                        </div>
+                        <div class="col-md-4">
+                            <label for="siteAbbre" class="form-label fw-bold">อักษรย่อไซต์งาน :</label>
+                            <input type="text" class="form-control" name="siteAbbre" id="siteAbbre" required>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-8">
+                            <button type="submit" class="btn btn-primary w-100" name="addSite"><i class="fa-solid fa-plus"></i> เพิ่ม</button>
+                        </div>
+                    </div>
+                </form>
+            </fieldset>
+        </div>
+    </section>  <!-- Closing Tag Container -->
+
+
+    <section class="container">
+        <div class="collapse show" id="collapseTable">
+            <fieldset class="p-3 shadow-sm mt-2">
+                <h5 class="fw-bold"><i class="fa-solid fa-table-list"></i> รายการไซต์งานทั้งหมด</h5>
+                <table class="table table-striped table-hover table-sm table-bordered css-serial">
+                    <thead class="bg-dark text-light">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">ชื่อไซต์งาน</th>
+                            <th scope="col">อักษรย่อ</th>
+                            <th scope="col">แก้ไข/อัพเดทข้อมูล</th>
+                        </tr>
+                    </thead>
+                    <tbody class="align-middle">
+
+                        <!-- query ตาราง ห้ามลบ -->
+                        <?php
+                            $stmt = $conn->query("SELECT * FROM site_info");
+                            $stmt->execute();
+                            $site = $stmt->fetchAll();
+
+                            if (!$site) {
+                                echo "<p><td colspan='4' class='text-center'>ไม่พบข้อมูล</td></p>";
+                            } else {
+                                foreach ($site as $fetch_siteInfo) {
+                        ?>
+
+                        <tr>
+                            <td></td>
+                            <td><?php echo $fetch_siteInfo['site_name']; ?></td>
+                            <td><?php echo $fetch_siteInfo['site_abbre']; ?></td>
+                            <td>
+                                <div class="btn-group" role="group" aria-label="Basic outlined example">
+                                    <button type="button" class="btn btn-sm btn-outline-dark" data-bs-toggle="modal" data-bs-target="#modalEditSite<?php echo $fetch_siteInfo['id']; ?>"><i class="fas fa-edit"></i></button>
+                                    <button type="button" class="btn btn-sm btn-outline-danger" data-bs-toggle="modal" data-bs-target="#modalDeleteSite<?php echo $fetch_siteInfo['id']; ?>"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+
+
+                        <!-- Modal แก้ไขข้อมูล ห้ามลบ -->
+                        <div class="modal fade" id="modalEditSite<?php echo $fetch_siteInfo['id']; ?>" tabindex="-1" aria-labelledby="modalEditSite" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modalEditSite"><i class="fa-solid fa-pen-to-square"></i> แก้ไขรายการไซต์งาน (Edit Sites)</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+
+                                    <div class="modal-body">
+                                        <form action="../db/db_siteManagement.php" method="POST">
+                                            <div class="mb-0">
+                                                <input type="hidden" class="form-control" name="id" value="<?php echo $fetch_siteInfo['id']; ?>" readonly required>
+                                                <label for="editSiteName" class="col-form-label">ชื่อไซต์งาน :</label>
+                                                <input type="text" class="form-control" name="editSiteName" id="editSiteName" value="<?php echo $fetch_siteInfo['site_name']; ?>" required>
+                                            </div>
+                                            <div class="mb-2">
+                                                <label for="editSiteAbbre" class="col-form-label">อักษรย่อไซต์งาน :</label>
+                                                <input type="text" class="form-control" name="editSiteAbbre" id="editSiteAbbre" value="<?php echo $fetch_siteInfo['site_abbre']; ?>" required>
+                                            </div>
+
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <button type="submit" class="btn btn-primary" name="editSite"><i class="fa-solid fa-floppy-disk"></i> Save</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <!-- Modal ยืนยันลบข้อมูล ห้ามลบ -->
+                        <div class="modal fade" id="modalDeleteSite<?php echo $fetch_siteInfo['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title"><i class="fas fa-trash"></i> ยืนยันลบรายการไซต์งาน ?</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <h6 class="text-center">ต้องการลบไซต์งานหรือไม่ ? กรุณายืนยัน</h6>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <a data-id="<?php echo $fetch_siteInfo['id']; ?>" href="?deleteSite=<?php echo $fetch_siteInfo['id']; ?>" class="btn btn-danger"><i class="fas fa-trash"></i> Delete</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <?php } } ?>        <!-- endforeach -->
+                    </tbody>
+                </table>
+            </fieldset>
+        </div>
+    </section>
+
+
+    <script>
+        // function calculateDays() {
+        //     var startDate = new Date(document.getElementById("projectStart").value);
+        //     var endDate = new Date(document.getElementById("projectEnd").value);
+        //     var timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
+        //     var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        //     document.getElementById("totalDays").value = diffDays;
+        // }
+    </script>
+
+
+</body>
+</html>
+
+<?php } ?>
