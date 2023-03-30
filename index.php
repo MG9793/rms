bf550c10497b4574d13fbaa365256e304a959bfe
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2023-03-30 12:30:37 +0700

    update 29MAR23

--- index.php
+++ index.php
@@ -101,6 +101,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="page/incomeRecord.php">บันทึกรายรับ</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">รายงาน</a>
+                    </li>
 
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-gears"></i> ตั้งค่า</a>
