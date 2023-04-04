73fe7190f08021a3d3d9df6c9f59ece977e605fe
BENZ <93135055+MG9793@users.noreply.github.com>
2023-04-05 02:53:25 +0700

    push

--- db/db_siteManagement.php
+++ db/db_siteManagement.php
@@ -6,8 +6,14 @@
 
     // เพิ่มข้อมูล item_info
     if (isset($_POST['addSite'])) {
-        $siteName = $_POST['siteName'];
-        $siteAbbre = $_POST['siteAbbre'];
+
+        $siteName = $_POST['SiteName'];
+        $siteAbbre = $_POST['SiteAbbre'];
+        $startDate = $_POST['StartDate'];
+        $finishDate = $_POST['FinishDate'];
+        $installment = $_POST['Installment'];
+        $total = $_POST['Total'];
+
 
         // check รายการซ้ำ
         $checkRow = $conn->query("SELECT site_name FROM site_info WHERE site_name = '$siteName'");
@@ -19,9 +25,13 @@
 
         // ถ้ารายการไม่ซ้ำ
         } else {
-            $stmt = $conn->prepare("INSERT INTO site_info(site_name, site_abbre) VALUES(:site_name, :site_abbre)");
+            $stmt = $conn->prepare("INSERT INTO site_info(site_name, site_abbre , start_date ,finish_date ,installment , total) VALUES(:site_name, :site_abbre , :start_date , :finish_date, :installment, :total)");
             $stmt->bindParam(":site_name", $siteName);
             $stmt->bindParam(":site_abbre", $siteAbbre);
+            $stmt->bindParam(":start_date", $startDate);
+            $stmt->bindParam(":finish_date", $finishDate);
+            $stmt->bindParam(":installment", $installment);
+            $stmt->bindParam(":total", $total);
             $stmt->execute();
 
             $_SESSION['addSite_success'] = '<i class="fa-solid fa-circle-check"></i> Success! บันทึกไซต์งานสำเร็จ';
@@ -35,26 +45,27 @@
         $id = $_POST['id'];
         $siteName = $_POST['editSiteName'];
         $siteAbbre = $_POST['editSiteAbbre'];
+        $startDate = $_POST['editStartDate'];
+        $finishDate = $_POST['editFinishDate'];
+        $installment = $_POST['editInstallment'];
+        $total = $_POST['editTotal'];
 
-        // check รายการซ้ำ
-        $checkRow = $conn->query("SELECT site_name FROM site_info WHERE site_name = '$siteName'");
-        if($checkRow->rowCount() >= 1) {
+        
 
-            // Alert รายการซ้ำ
-            $_SESSION['addSite_error'] = '<i class="fa-solid fa-circle-check"></i> Error! ไซต์งานนี้ได้บันทึกอยู่ในระบบแล้ว กรุณาตรวจสอบอีกครั้ง';
-            header("location: ../page/siteManagement.php");
 
-        // ถ้ารายการไม่ซ้ำ
-        } else {
-            $stmt = $conn->prepare("UPDATE site_info SET site_name = :site_name, site_abbre = :site_abbre WHERE id = :id");
+            $stmt = $conn->prepare("UPDATE site_info SET site_name = :site_name, site_abbre = :site_abbre ,start_date = :start_date,finish_date = :finish_date,installment = :installment , total = :total WHERE id = :id");
             $stmt->bindParam(":id", $id);
             $stmt->bindParam(":site_name", $siteName);
             $stmt->bindParam(":site_abbre", $siteAbbre);
+            $stmt->bindParam(":start_date", $startDate);
+            $stmt->bindParam(":finish_date", $finishDate);
+            $stmt->bindParam(":installment", $installment);
+            $stmt->bindParam(":total", $total);
             $stmt->execute();
 
-            $_SESSION['editSite_success'] = '<i class="fa-solid fa-circle-check"></i> Success! แก้ไขไซต์งานสำเร็จ';
+           
             header("location: ../page/siteManagement.php");
-        }
+        
     }
 
 ?>
