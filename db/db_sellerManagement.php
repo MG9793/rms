0a1a0961f5b0afb791a9c9db56443b7dc6358ec9
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2023-06-14 14:20:08 +0700

    อัพเดทหลังประชุมงานกับลูกค้า 14JUN23

--- db/db_sellerManagement.php
+++ db/db_sellerManagement.php
@@ -4,14 +4,14 @@
     require_once 'config/conn.php';
 
 
-    // เพิ่มข้อมูล item_info
+    // เพิ่มข้อมูลจากหน้าตั้งค่า
     if (isset($_POST['addSales'])) {
         $salesName = $_POST['salesName'];
         $salesBranch = $_POST['salesBranch'];
         $taxNo = $_POST['taxNo'];
 
         if ($salesBranch == "") {
-            $salesBranch = "สำนักงานใหญ่";
+            $salesBranch = $salesName . " (สำนักงานใหญ่)";
 
             $stmt = $conn->prepare("INSERT INTO sales_info(sales_name, sales_branch, tax_no) VALUES(:sales_name, :sales_branch, :tax_no)");
             $stmt->bindParam(":sales_name", $salesName);
@@ -23,6 +23,7 @@
             header("location: ../page/sellerManagement.php");
 
         } else {
+            $salesBranch = $salesName . " ($salesBranch)";
 
             $stmt = $conn->prepare("INSERT INTO sales_info(sales_name, sales_branch, tax_no) VALUES(:sales_name, :sales_branch, :tax_no)");
             $stmt->bindParam(":sales_name", $salesName);
@@ -37,7 +38,7 @@
     }
 
 
-    // แก้ไขข้อมูล item_info
+    // แก้ไขข้อมูลจากหน้าตั้งค่า
     else if (isset($_POST['editSales'])) {
         $id = $_POST['id'];
         $salesName = $_POST['editSalesName'];
@@ -54,6 +55,42 @@
         $_SESSION['editSales_success'] = '<i class="fa-solid fa-circle-check"></i> Success! แก้ไขผู้ขายสำเร็จ';
         header("location: ../page/sellerManagement.php");
 
+    }
+
+
+    // เพิ่มข้อมูลจากหน้าบันทึกรายจ่าย
+    else if (isset($_POST['add_QuickSeller'])) {
+        $salesName = $_POST['addSalesName'];
+        $salesBranch = $_POST['addSalesBranch'];
+        $taxNo = $_POST['addtaxNo'];
+
+        if ($salesBranch == "") {
+            $salesBranch = $salesName . " (สำนักงานใหญ่)";
+
+            $stmt = $conn->prepare("INSERT INTO sales_info(sales_name, sales_branch, tax_no) VALUES(:sales_name, :sales_branch, :tax_no)");
+            $stmt->bindParam(":sales_name", $salesName);
+            $stmt->bindParam(":sales_branch", $salesBranch);
+            $stmt->bindParam(":tax_no", $taxNo);
+            $stmt->execute();
+
+            $_SESSION['addSuccess'] = '<i class="fa-solid fa-circle-check"></i> Success! บันทึกข้อมูลสำเร็จ';
+            header("location: ../page/expense.php");
+
+
+        } else {
+            $salesBranch = $salesName . " ($salesBranch)";
+
+            $stmt = $conn->prepare("INSERT INTO sales_info(sales_name, sales_branch, tax_no) VALUES(:sales_name, :sales_branch, :tax_no)");
+            $stmt->bindParam(":sales_name", $salesName);
+            $stmt->bindParam(":sales_branch", $salesBranch);
+            $stmt->bindParam(":tax_no", $taxNo);
+            $stmt->execute();
+
+            $_SESSION['addSuccess'] = '<i class="fa-solid fa-circle-check"></i> Success! บันทึกข้อมูลสำเร็จ';
+            header("location: ../page/expense.php");
+
+
+        }
     }
 
 ?>
--- db/db_siteManagement.php
+++ db/db_siteManagement.php
@@ -4,10 +4,16 @@
     require_once 'config/conn.php';
 
 
-    // เพิ่มข้อมูล item_info
+    // เพิ่มข้อมูลจากหน้าตั้งค่า
     if (isset($_POST['addSite'])) {
-        $siteName = $_POST['siteName'];
-        $siteAbbre = $_POST['siteAbbre'];
+
+        $siteName = $_POST['SiteName'];
+        $siteAbbre = $_POST['SiteAbbre'];
+        $startDate = $_POST['StartDate'];
+        $finishDate = $_POST['FinishDate'];
+        $installment = $_POST['Installment'];
+        $total = $_POST['Total'];
+
 
         // check รายการซ้ำ
         $checkRow = $conn->query("SELECT site_name FROM site_info WHERE site_name = '$siteName'");
@@ -19,9 +25,13 @@
 
         // ถ้ารายการไม่ซ้ำ
         } else {
-            $stmt = $conn->prepare("INSERT INTO site_info(site_name, site_abbre) VALUES(:site_name, :site_abbre)");
+            $stmt = $conn->prepare("INSERT INTO site_info(site_name, site_abbre , start_date ,finish_date ,installment , total) VALUES(:site_name, :site_abbre , :start_date , :finish_date, :installment, :total)");
             $stmt->bindParam(":site_name", $siteName);
             $stmt->bindParam(":site_abbre", $siteAbbre);
+            $stmt->bindParam(":start_date", $startDate);
+            $stmt->bindParam(":finish_date", $finishDate);
+            $stmt->bindParam(":installment", $installment);
+            $stmt->bindParam(":total", $total);
             $stmt->execute();
 
             $_SESSION['addSite_success'] = '<i class="fa-solid fa-circle-check"></i> Success! บันทึกไซต์งานสำเร็จ';
@@ -30,31 +40,65 @@
     }
 
 
-    // แก้ไขข้อมูล item_info
+    // แก้ไขข้อมูลจากหน้าตั้งค่า
     else if (isset($_POST['editSite'])) {
         $id = $_POST['id'];
         $siteName = $_POST['editSiteName'];
         $siteAbbre = $_POST['editSiteAbbre'];
+        $startDate = $_POST['editStartDate'];
+        $finishDate = $_POST['editFinishDate'];
+        $installment = $_POST['editInstallment'];
+        $total = $_POST['editTotal'];
+
+        $stmt = $conn->prepare("UPDATE site_info SET site_name = :site_name, site_abbre = :site_abbre ,start_date = :start_date,finish_date = :finish_date,installment = :installment , total = :total WHERE id = :id");
+        $stmt->bindParam(":id", $id);
+        $stmt->bindParam(":site_name", $siteName);
+        $stmt->bindParam(":site_abbre", $siteAbbre);
+        $stmt->bindParam(":start_date", $startDate);
+        $stmt->bindParam(":finish_date", $finishDate);
+        $stmt->bindParam(":installment", $installment);
+        $stmt->bindParam(":total", $total);
+        $stmt->execute();
+
+        header("location: ../page/siteManagement.php");
+        
+    }
+
+
+    // เพิ่มข้อมูลจากหน้าบันทึกรายจ่าย
+    else if (isset($_POST['add_QuickSite'])) {
+        $siteName = $_POST['siteName'];
+        $siteAbbre = $_POST['siteAbbre'];
+        $startDate = $_POST['startDate'];
+        $finishDate = $_POST['finishDate'];
+        $installment = $_POST['addInstallment'];
+        $total = $_POST['addTotal'];
 
         // check รายการซ้ำ
         $checkRow = $conn->query("SELECT site_name FROM site_info WHERE site_name = '$siteName'");
         if($checkRow->rowCount() >= 1) {
-
+        
             // Alert รายการซ้ำ
-            $_SESSION['addSite_error'] = '<i class="fa-solid fa-circle-check"></i> Error! ไซต์งานนี้ได้บันทึกอยู่ในระบบแล้ว กรุณาตรวจสอบอีกครั้ง';
-            header("location: ../page/siteManagement.php");
-
+            $_SESSION['duplicateData'] = '<i class="fa-solid fa-circle-check"></i> Error! ไซต์งานนี้ได้บันทึกอยู่ในระบบแล้ว กรุณาตรวจสอบอีกครั้ง';
+            header("location: ../page/expense.php");
+        
         // ถ้ารายการไม่ซ้ำ
         } else {
-            $stmt = $conn->prepare("UPDATE site_info SET site_name = :site_name, site_abbre = :site_abbre WHERE id = :id");
-            $stmt->bindParam(":id", $id);
+
+            $stmt = $conn->prepare("INSERT INTO site_info(site_name, site_abbre , start_date ,finish_date ,installment , total) VALUES(:site_name, :site_abbre , :start_date , :finish_date, :installment, :total)");
             $stmt->bindParam(":site_name", $siteName);
             $stmt->bindParam(":site_abbre", $siteAbbre);
+            $stmt->bindParam(":start_date", $startDate);
+            $stmt->bindParam(":finish_date", $finishDate);
+            $stmt->bindParam(":installment", $installment);
+            $stmt->bindParam(":total", $total);
             $stmt->execute();
 
-            $_SESSION['editSite_success'] = '<i class="fa-solid fa-circle-check"></i> Success! แก้ไขไซต์งานสำเร็จ';
-            header("location: ../page/siteManagement.php");
+            $_SESSION['addSuccess'] = '<i class="fa-solid fa-circle-check"></i> Success! บันทึกข้อมูลสำเร็จ';
+            header("location: ../page/expense.php");
+
         }
+
     }
 
 ?>
