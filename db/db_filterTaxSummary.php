5a13a57d5da7174109ac68183cb29c9b892afce1
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2023-06-17 23:29:12 +0700

    UPDATE 17JUN23

--- db/db_filterTaxSummary.php
+++ db/db_filterTaxSummary.php
@@ -4,35 +4,45 @@
     require_once 'config/conn.php';
 
 
-    if (isset($_POST['selectCompany'])) {
-        $getCompany = $_POST['selectCompany'];
-        $getTime = $_POST['selectTime'];
+    // if (isset($_POST['selectCompany'])) {
+    //     $getCompany = $_POST['selectCompany'];
+    //     $getTime = $_POST['selectTime'];
 
-        $stmt = $conn->query("SELECT * FROM company_info WHERE company_name = '$getCompany'");
-        $companyInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+    //     $stmt = $conn->query("SELECT * FROM company_info WHERE company_name = '$getCompany'");
+    //     $companyInfo = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        $_SESSION['selectTime'] = $getTime;
-        $_SESSION['selectCompany'] = $getCompany;
-        $_SESSION['companyTax'] = $companyInfo['tax_no'];
+    //     $_SESSION['selectTime'] = $getTime;
+    //     $_SESSION['selectCompany'] = $getCompany;
+    //     $_SESSION['companyTax'] = $companyInfo['tax_no'];
 
-        header("location: ../report/filterTaxSummary.php");
+    //     header("location: ../report/filterTaxSummary.php");
 
-    }
+    // }
 
     
-    else if (isset($_POST['sendToReport'])) {
+    if (isset($_POST['sendReport'])) {
 
         $receipt_nos = $_POST['receipt_no'];
         $reports = $_POST['report'];        // ดึงมาจาก form checkbox
+
+        if (empty($reports)) {
+            $_SESSION['reportFailure'] = '<i class="fa-solid fa-triangle-exclamation"></i> Error! กรุณาเลือกรายการออกใบกำกับภาษี';
+            header("location: ../report/filterTaxSummary.php");
+
+        } else {
     
-        foreach ($receipt_nos as $receipt_no) {
-            if (isset($reports[$receipt_no]) && $reports[$receipt_no] === 'Y') {
-                $stmt = $conn->prepare("UPDATE bill_head SET report = 'Y' WHERE receipt_no = :receipt_no");
-                $stmt->execute(['receipt_no' => $receipt_no]);
+            foreach ($receipt_nos as $receipt_no) {
+                if (isset($reports[$receipt_no]) && $reports[$receipt_no] === 'Y') {
+                    $stmt = $conn->prepare("UPDATE bill_head SET report = 'Y' WHERE receipt_no = :receipt_no");
+                    $stmt->execute(['receipt_no' => $receipt_no]);
+
+                    header("location: ../report/taxReport.php");
+
+                }
             }
         }
 
-        header("location: ../report/taxReport.php");
+        //header("location: ../report/taxReport.php");
     }
 
 
